4a2aa1e963f27ef9a8e34675ddbfef8b19f1dffa
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-27 23:02:26 +0300

    task 10

---
+++ task 10/Program.cs
@@ -0,0 +1,6 @@
+﻿
+System.Console.WriteLine("ВВедите трёхзначное число: ");
+int num1 =Convert.ToInt32(Console.ReadLine());
+int result = num1 / 10;
+result = result % 10;
+System.Console.WriteLine($"Второе число - {result}");
