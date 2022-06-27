aef2dfde8d35e0c24e11d263e7cdce740b177900
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-27 23:52:51 +0300

    task 13

---
+++ task 13/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 3215687;
+int result = 0;
+
+if (number/100 == 0)
+{
+    System.Console.WriteLine($"В числе {number} нет третьей цифры!");
+}
+while (number/1000 != 0)
+{
+    number = number / 10;
+    result = number % 10;
+}
+
+System.Console.Write("третья цифра ");
+System.Console.WriteLine(result);
