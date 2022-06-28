02dfa7676c5b2d27c37d37296adffd5ba6541139
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-28 22:09:03 +0300

    task 15

---
+++ task 15/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("ВВедите день недели (от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    System.Console.WriteLine("Это выходной");
+}
+else if (0 < day && day < 6)
+{
+    System.Console.WriteLine("Это буднии день");
+}
+else
+{
+    System.Console.WriteLine("Неверный ввод");
+}
